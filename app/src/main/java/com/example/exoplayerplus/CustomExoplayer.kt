b71eb6a3892d105677497adde05b894afd6eea06
Bharat-HoiChoi <139767455+Bharat-HoiChoi@users.noreply.github.com>
2023-11-01 07:34:38 +0530

    Started project made basic designs

---
+++ app/src/main/java/com/example/exoplayerplus/CustomExoplayer.kt
@@ -0,0 +1,181 @@
+package com.example.exoplayerplus
+
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.media3.common.MediaItem
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.ui.PlayerView
+
+
+@Composable
+fun CustomExoplayer(modifier: Modifier = Modifier) {
+
+    val context = LocalContext.current
+
+    var areControlsVisible by remember {
+        mutableStateOf(false)
+    }
+
+    val exoPlayer = remember {
+        ExoPlayer.Builder(context).build().apply {
+            setMediaItem(
+                MediaItem.fromUri(
+                    "https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4"
+                )
+            )
+            prepare()
+
+            playWhenReady = true
+        }
+    }
+
+    Box(modifier = modifier.aspectRatio(16 / 9f)) {
+        DisposableEffect(key1 = Unit) { onDispose { exoPlayer.release() } }
+
+        AndroidView(
+            modifier = Modifier.clickable {
+                areControlsVisible = !areControlsVisible
+            },
+            factory = {
+                PlayerView(context).apply {
+                    player = exoPlayer
+                    useController = false
+                    layoutParams =
+                        FrameLayout.LayoutParams(
+                            ViewGroup.LayoutParams.MATCH_PARENT,
+                            ViewGroup.LayoutParams.MATCH_PARENT
+                        )
+
+                }
+            }
+        )
+        Column(modifier = Modifier.fillMaxSize()) {
+            AnimatedVisibility(
+                visible = areControlsVisible,
+                enter = fadeIn(), exit = fadeOut()
+            ) {
+                CenterPlayerControls(onReplayClick = { /*TODO*/ }, onPlayPauseToggle = {
+
+                }, onForwardClick = {
+
+                }, onBrightnessChange = {
+
+                }, brightnessLevel = 50f)
+            }
+        }
+
+
+    }
+
+}
+
+@Composable
+fun CenterPlayerControls(
+    modifier: Modifier = Modifier,
+    onReplayClick: () -> Unit = {},
+    onPlayPauseToggle: (isPlaying: Boolean) -> Unit = {},
+    onForwardClick: () -> Unit = {},
+    onBrightnessChange: (value: Float) -> Unit = {},
+    brightnessLevel: Float,
+) {
+    val isPlayingValue by remember {
+        mutableStateOf(false)
+    }
+
+
+    //black overlay across the video player
+    Box(modifier = modifier.background(Color.Black.copy(alpha = 0.6f))) {
+        Row(
+            modifier = Modifier
+                .align(Alignment.Center)
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+
+            //Brightness Slider
+            Column {
+                IconButton(onClick = { }) {
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_brightness_medium),
+                        contentDescription = "Brightness Level"
+                    )
+                }
+
+            }
+
+            //replay button
+            IconButton(modifier = Modifier.size(40.dp), onClick = {
+                onReplayClick()
+            }) {
+                Image(
+                    modifier = Modifier.fillMaxSize(),
+                    contentScale = ContentScale.Crop,
+                    painter = painterResource(id = R.drawable.ic_exo_icon_rewind),
+                    contentDescription = "Replay 15 seconds"
+                )
+            }
+
+            //pause/play toggle button
+            IconButton(modifier = Modifier.size(40.dp), onClick = {
+                onPlayPauseToggle(isPlayingValue)
+            }) {
+                Image(
+                    modifier = Modifier.fillMaxSize(),
+                    contentScale = ContentScale.Crop,
+                    painter = if (isPlayingValue) painterResource(id = R.drawable.ic_play_triangle) else painterResource(
+                        id = R.drawable.ic_pause
+                    ),
+                    contentDescription = "Play/Pause"
+                )
+            }
+
+            //forward button
+            IconButton(modifier = Modifier.size(40.dp), onClick = {
+                onForwardClick()
+            }) {
+                Image(
+                    modifier = Modifier.fillMaxSize(),
+                    contentScale = ContentScale.Crop,
+                    painter = painterResource(id = R.drawable.ic_exo_icon_fastforward),
+                    contentDescription = "Forward 10 seconds"
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PlayerControlsPreview() {
+    CenterPlayerControls(brightnessLevel = 50f)
+}
---
+++ app/src/main/java/com/example/exoplayerplus/Slider.kt
@@ -0,0 +1,91 @@
+package com.example.exoplayerplus
+
+import androidx.annotation.FloatRange
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Slider
+import androidx.compose.material3.SliderDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.TransformOrigin
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.layout.layout
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Constraints
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HSlider(@FloatRange(from = 0.0, to = 1.0) defaultValue: Float, onValueChange: (Float) -> Unit,sliderStyle: SliderStyle = SliderStyle.DefaultSliderStyle) {
+    Column {
+        var sliderPosition by remember { mutableStateOf(defaultValue) }
+        Slider(
+            modifier = Modifier
+                .graphicsLayer {
+                    rotationZ = 270f
+                    transformOrigin = TransformOrigin(0f, 0f)
+                }
+                .layout { measurable, constraints ->
+                    val placeable = measurable.measure(
+                        Constraints(
+                            minWidth = constraints.minHeight,
+                            maxWidth = constraints.maxHeight,
+                            minHeight = constraints.minWidth,
+                            maxHeight = constraints.maxHeight,
+                        )
+                    )
+                    layout(placeable.height, placeable.width) {
+                        placeable.place(-placeable.width, 0)
+                    }
+                }.height(sliderStyle.sliderWidth),
+            value = sliderPosition,
+            onValueChange = {
+                onValueChange(sliderPosition)
+                sliderPosition = it
+            },
+            thumb = {
+                SliderDefaults.Thumb(
+                    interactionSource = MutableInteractionSource(),
+                    thumbSize = DpSize.Zero
+                )
+            },
+            colors = SliderDefaults.colors(
+                activeTrackColor = sliderStyle.activeTrackColor,
+                inactiveTrackColor = sliderStyle.inactiveTrackColor
+            ),
+            track = {
+                SliderDefaults.Track(
+                    sliderPositions = it,
+                    modifier = Modifier.height(20.dp),
+                )
+            }
+
+        )
+    }
+
+}
+
+@Preview
+@Composable
+fun HSliderPrev() {
+    HSlider(0.4f, {})
+}
+
+data class SliderStyle(
+    val sliderWidth:Dp,
+    val activeTrackColor: Color,
+    val inactiveTrackColor: Color,
+){
+    companion object {
+        val DefaultSliderStyle = SliderStyle(50.dp, Color.Blue, Color.Gray)
+    }
+}
