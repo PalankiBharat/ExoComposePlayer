337588fd00f412d555d28655afd4b8dac71a8aba
Bharat-sdk <bk9735732777@gmail.com>
2023-11-06 17:01:45 +0530

    Added tap to forward animation

--- app/src/main/java/com/example/exoplayerplus/DoubleTapToForwardAnimation.kt
+++ app/src/main/java/com/example/exoplayerplus/DoubleTapToForwardAnimation.kt
@@ -1,113 +1,199 @@
 package com.example.exoplayerplus
 
 
+import android.util.Log
 import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.FastOutLinearInEasing
 import androidx.compose.animation.core.LinearEasing
-import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.tween
-import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.rotate
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun DoubleTapToForwardIcon() {
+fun DoubleTapToForwardIcon(isForward:Boolean = true,onClick: () -> Unit = {}) {
+
+
     val rotation by remember {
         mutableStateOf(Animatable(0f))
     }
 
-    var enabled by remember { mutableStateOf(true) }
-    val margin by animateDpAsState(targetValue = if (enabled) 5.dp else 100.dp, label = "", animationSpec = tween(1000, easing = LinearEasing))
 
-
-    LaunchedEffect(Unit) {
-        rotation.animateTo(
-            targetValue = 360f,
-            animationSpec = tween(500, easing = LinearEasing)
-        )
-
-    }
-    LaunchedEffect (Unit){
-        enabled = false
-
+    val sliding by remember {
+        mutableStateOf(Animatable(0f))
     }
 
-    LaunchedEffect (enabled){
-        if (!enabled)
+    val alpha by remember {
+        mutableStateOf(Animatable(100f))
+    }
+
+
+    var animationRunning by remember { mutableStateOf(false) }
+
+    var height by remember {
+        mutableIntStateOf(0)
+    }
+
+    LaunchedEffect(height) {
+        Log.d("TAG", "DoubleTapToForwardIcon: $height")
+    }
+
+
+    LaunchedEffect(animationRunning) {
+        if (animationRunning)
         {
-            enabled = true
+            rotation.animateTo(
+                targetValue =  60f,
+                animationSpec = tween(durationMillis = 250, easing = FastOutLinearInEasing),
+            )
         }
-
+    }
+    LaunchedEffect(animationRunning) {
+        if (animationRunning)
+        {
+            sliding.animateTo(
+                targetValue = 100f,
+                animationSpec = tween(500, easing = LinearEasing)
+            )
+        }
+    }
+    LaunchedEffect(animationRunning) {
+        if (animationRunning) {
+            alpha.animateTo(
+                targetValue = 0f,
+                animationSpec = tween(500, easing = LinearEasing)
+            )
+        }
     }
 
-    /*  Box {
-          Canvas(modifier = Modifier
-              .fillMaxSize()
-              .aspectRatio(1f)
-              , contentDescription = "Double tap to forward") {
-              val width = this.size.width
-              val height = this.size.height
-
-              val path = Path().apply {
-                  drawArc(color = Color.White, useCenter = false, startAngle = 0f, sweepAngle = 300f, style = Stroke(width = 8f), size = Size(width*0.8f,height*0.8f), topLeft = Offset((width-(width*0.8f))/2,(height-(height*0.8f))/2) )
-                  moveTo(center.x,center.y)
-
-              }
-              val measuredText =
-                  textMeasurer.measure(
-                      AnnotatedString("10"),
-                      constraints = Constraints.fixed(
-                          width = (size.width ).toInt(),
-                          height = (size.height).toInt()
-                      ),
-                      style = TextStyle(color = Color.White)
-                  )
-              drawCircle(radius = width/2,center= center, style = Stroke(width = 8f), color = Color.White)
-              drawArc(color = Color.White, useCenter = false, startAngle = 0f, sweepAngle = 300f, style = Stroke(width = 8f), size = Size(width*0.8f,height*0.8f), topLeft = Offset((width-(width*0.8f))/2,(height-(height*0.8f))/2) )
-
-              //drawText(textMeasurer = textMeasurer, text = "10", topLeft = center,style = TextStyle(color = Color.White, fontSize = 16.sp) )
-              drawText (measuredText, topLeft = center)
-          }
-      }*/
-
-    Box(Modifier.fillMaxWidth()) {
-        Icon(
-            modifier = Modifier
-                .rotate(rotation.value)
-                .align(Alignment.TopStart),
-            contentDescription = "",
-            painter = painterResource(id = R.drawable.forward_only)
-        )
-        Row (modifier = Modifier
-            .fillMaxWidth()
-            .align(Alignment.CenterStart)){
-            Text(text = "10", modifier = Modifier
-                .padding(start = margin))
+    LaunchedEffect(sliding.value) {
+        if (!sliding.isRunning) {
+            sliding.snapTo(0f)
+            alpha.snapTo(100f)
+            animationRunning = false
         }
-
     }
+
+    LaunchedEffect(rotation.value) {
+        if (!rotation.isRunning)
+        {
+            rotation.animateTo(
+                targetValue = 0f,
+                animationSpec = tween(durationMillis = 60, easing = FastOutLinearInEasing),
+            )
+        }
+    }
+
+    Box(modifier = Modifier.clickable {
+        animationRunning = true
+        onClick()
+    }) {
+        Box(modifier = Modifier
+            .wrapContentHeight()
+            .run {
+                if (!isForward) {
+                    this.align(Alignment.CenterEnd)
+                } else {
+                    this
+                }
+            }
+            .onGloballyPositioned {
+                height = it.size.height
+            }
+
+        ) {
+            Icon(
+                modifier = Modifier
+                    .run {
+                        if (!isForward) {
+                            this.graphicsLayer {
+                                rotationY = 180f
+                            }
+                        } else {
+                            this
+                        }
+
+                    }
+                    .rotate(-60f)
+                    .rotate(rotation.value)
+                    .align(Alignment.Center),
+                contentDescription = "",
+
+                painter = painterResource(id = R.drawable.forward_only)
+            )
+            if (alpha.value == 100f) {
+                Text(
+                    text = "10", modifier = Modifier
+                        .align(Alignment.Center)
+                        .padding(4.dp)
+                )
+            }
+        }
+        if (alpha.value < 100) {
+            Box(
+                modifier = Modifier
+                    .run {
+                        if (!isForward) {
+                            this.align(Alignment.CenterEnd)
+                        } else {
+                            this
+                        }
+                    }
+                    .height(height = with(LocalDensity.current) { height.toDp() })
+            ) {
+                Row(
+                    modifier = Modifier
+                        .alpha(alpha.value / 100)
+                        .align(Alignment.Center)
+                ) {
+                    Text(
+                        text =if (isForward) "++10" else "--10", modifier = Modifier.run {
+                            if (isForward) {
+                                this.padding(start = 14.dp + sliding.value.toInt().dp)
+                            }
+                                this.padding(end = 14.dp + sliding.value.toInt().dp)
+                        })
+                }
+            }
+        }
+    }
+
 
 }
 
-@Preview
+@Preview(backgroundColor = 0xFFFFFFFF, showBackground = true)
 @Composable
 private fun DoubleTapToForwardIconPreview() {
-    DoubleTapToForwardIcon()
+    Box(modifier = Modifier.fillMaxWidth()) {
+        Box(
+            Modifier
+                .align(Alignment.CenterEnd)
+        ) {
+            DoubleTapToForwardIcon(isForward = false)
+        }
+    }
 }
 
