76f7967f51a425350bc17d8081d21ec7297a5559
Bharat-HoiChoi <139767455+Bharat-HoiChoi@users.noreply.github.com>
2023-12-27 21:46:54 +0530

    :bug: fixed bugs :bug:

--- exoplayer-plus/build.gradle.kts
+++ exoplayer-plus/build.gradle.kts
@@ -48,7 +48,7 @@ android {
 
 dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.8.1")
+    implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2023.10.01"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.material3:material3")
@@ -71,7 +71,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.github.PalankiBharat"
             artifactId = "ExoComposePlayer"
-            version = "0.1.9"
+            version = "0.2.0"
 
             afterEvaluate {
                 from(components["release"])
